b82cfd22abb80fcf934cdb6644a68cdce7c20273
lamquochuy <lamquochuy.ct@gmail.com>
2015-06-12 16:38:07 +0700

    about master

---
+++ about-master.php
@@ -0,0 +1,13 @@
+<?php
+
+Cach t?o SSH Key ch?ng nh?n nh? sau: M? Git Bash ?a ???c cai ??t ? tren:
+
+Step1: $ ssh-keygen -t dsa : kh?i t?o SSH key
+Step 2: Ch?n ???ng d?n cho file SSH key: C:/Users/your_computer_name/.ssh/id_dsa
+Step 3: Nh?p m?t password c?a b?n (nh?p 2 l?n)
+Step 4: B?n ?a t?o key thanh cong key s? n?m t?i ??a ch? C:/Users/your_computer_name/.ssh/id_dsa
+
+Step 5: Add SSH Key local vao server GitHub: ??n ??a ch? file SSH key ma b?n ?a t?o, m? file id_dsa.pub va copy t?t c? n?i dung key trong ?o
+aAs an open source project, Git for Windows benefits greatly from both the volunteer work of helpful developers and good bug reports made by Git for Windows' users.
+Bug Reports & Feature Requests
+If you've noticed a bug or simply have an idea that you'd like to see come real, why not work on it? Bug reports and feature requests are typically submitted to the issue tracker on our GitHub repository.
